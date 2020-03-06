0578a1332c9c8e0f0ba60acf536051d0e288d3dc
Derek Johnson <wars2cool@gmail.com>
2020-03-07 00:58:59 +0900

    Worked on books and user books controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,30 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.create(book_params)
+  end
+
+  def update
+    @book.update(book_params)
+  end
+
+  def find_book
+    @book = Book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :total_books, :checked_books, :cost_day)
+  end
 end
--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -1,2 +1,26 @@
 class UserBooksController < ApplicationController
+  def new
+    @user_book = User_Book.new
+  end
+
+  def create
+    @user_book = User_Book.new(user_book_params)
+    @user_book.user = current_user
+    @user_book.book = Book.find(params[:book_id])
+  end
+
+  def find_book
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @userbook = User_Book.find(params[:id])
+    @userbook.destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:user_book).permit()
+  end
 end
