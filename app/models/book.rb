8a076c051cbf0a6a1c1b463d6556b5e21c4e669e
Derek Johnson <wars2cool@gmail.com>
2020-03-08 01:13:48 +0900

    updated models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  has_many :user_books, dependent: :destroy
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :total_books, presence: true
+  validates :cost_day, presence: true
 end
--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -1,2 +1,5 @@
 class UserBook < ApplicationRecord
+  belongs_to :book
+  belongs_to :user
+  validates :checkout_date, presence: true
 end
